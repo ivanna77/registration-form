dc784b60fd30bf9564e19dd6935fc06ad5a16f14
Ivanna Makarchuk <ivanna_makarchuk@ukr.net>
2022-10-14 04:15:34 +0300

    Fix issue with page reloading (e.preventDefault)

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-//alert("This alert box was called with the onload event")
-document.getElementById("submit").onclick = function() {  
-    register()  
+document.getElementById("submit").onclick = function(e) {  
+    register(e)  
 }; 
-function register() {  
+function register(e) {
+    email_error = document.getElementById("email-error");
+    email_error.innerHTML = "";  // clear email error label
+
+    psw_error = document.getElementById("psw-error");
+    psw_error.innerHTML = "";  // clear psw error label
+
+    psw_repeat_error = document.getElementById("psw-repeat-error");
+    psw_repeat_error.innerHTML = "";  // clear psw-repeat error label
+
     email = document.getElementById("email").value.trim();
-    psw = document.getElementById("psw").value;
-    psw_repeat = document.getElementById("psw-repeat").value;
+    psw = document.getElementById("psw").value.toString();
+    psw_repeat = document.getElementById("psw-repeat").value.toString();
     
     if (typeof email === 'string' && email.length === 0) {
-        document.getElementById("email-error").innerHTML = "Please, enter an email.";  
+        email_error.innerHTML = "Please, enter an email.";  
+        e.preventDefault();
+        e.returnValue = '';
         return;
     }
     if (typeof psw === 'string' && psw.length < 8) {
-        document.getElementById("psw-error").innerHTML = "Password must be at least 8 symbols.";  
+        psw_error.innerHTML = "Password must be at least 8 symbols.";  
+        e.preventDefault();
+        e.returnValue = '';
         return;
     }
-    if (psw.localeCompare(psw_repeat) != 0) {
-        document.getElementById("psw-repeat-error").innerHTML = "Password mismatch.";  
+    if (psw !== psw_repeat) {
+        psw_repeat_error.innerHTML = "Password mismatch.";  
+        e.preventDefault();
+        e.returnValue = '';
         return;
     }
-
-    alert("email: " + email + "\npsw: " + psw+ "\npsw-repeat: " + psw_repeat);
+    
     document.getElementById("submit").innerHTML = "Crimea is Ukraine <3!";  
     document.getElementById("submit").style.color = "blue";  
     document.getElementById("submit").style.backgroundColor = "yellow";  
     document.getElementById("submit").style.fontSize = "25px";  
     document.getElementById("submit").style.border = "4px solid red";
+    
+    e.preventDefault();
+    e.returnValue = '';    
 }
 
